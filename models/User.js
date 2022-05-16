d796e8557bba6e1fa7096e247fdec49a24832c94
Rachel Gray <rachel.gray7448@gmail.com>
2022-05-15 21:44:38 -0400

    set up comment model and serverjs

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const sequelize = require('./config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+const sequelize = require('./config/connection')
+const routes = require('./controllers');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequellize')(session.Store);
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+const sess = {
+    secret: "secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("New listening"));
+});
